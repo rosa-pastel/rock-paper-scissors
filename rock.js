b5df2271e791d9224805b6df3d25075934151d6d
zeynepmimutlu <zeynepranamutlu@outlook.com>
2021-08-02 17:59:33 +0300

    added very simple UI to display winners on page

--- rock.js
+++ rock.js
@@ -12,54 +12,61 @@ function computerPlay(){
     }
     return computerSelection
 }
-function playRound(playerSelection,computerSelection){
-    playerSelection=playerSelection.toLowerCase()
-    let roundWinner
+function playRound(playerSelection){
+    let computerSelection=computerPlay()
     if (playerSelection==computerSelection){
-        roundWinner="tie"
+        return "tie"
     }
     else if (playerSelection=="rock"){
-        roundWinner=(computerSelection=="paper") ? "computer":"player"
+        return (computerSelection=="paper") ? "computer":"player"
     }
     else if (playerSelection="paper"){
-        roundWinner=(computerSelection=="scissors") ? "computer":"player"
+        return (computerSelection=="scissors") ? "computer":"player"
     }
     else if (playerSelection="scissors"){
-        roundWinner=(computerSelection=="rock") ? "computer":"player"
+        return (computerSelection=="rock") ? "computer":"player"
     }
-    return roundWinner
 }
 function game(){
     let playerPoints=0
     let computerPoints=0
+    let roundWinner
     let roundMessage
-    for (let round=1; round<6; round++){
-        let playerSelection=prompt("Rock, paper or scissors?")
-        let computerSelection=computerPlay()
-        let roundWinner=playRound(playerSelection,computerSelection)
-        console.log(roundMessage=(roundWinner=="tie") ? ("Tie!"):("Winner of the round is "+roundWinner+"!")) 
-        switch (roundWinner){
-            case "computer":
-                computerPoints++
-                break
-            case "player":
-                playerPoints++
-                break
-            case  "tie":
-                break
-        }
+    const roundResult=document.querySelector('#roundresult')
+    const gameResult=document.querySelector('#gameresult')
+    const btn=document.querySelectorAll('#choicebutton')
+    for(let i=0; i<btn.length; i++){
+        btn[i].addEventListener('click', () => {
+            if (playerPoints<5 && computerPoints<5){
+            roundWinner = playRound(btn[i].innerText.toLowerCase())
+            roundMessage = (roundWinner=="tie") ? ("Tie!"):("Winner of the round is "+roundWinner+"!")
+            roundResult.innerText=roundMessage
+                switch (roundWinner){
+                    case "computer":
+                        computerPoints++
+                        break
+                    case "player":
+                        playerPoints++
+                        break
+                    case  "tie":
+                        break
+                }
+            }
+            else{
+                let gameWinnerMessage
+                if (computerPoints==playerPoints){
+                    gameWinnerMessage="Tie! No winner."
+                }
+                else if (computerPoints>playerPoints){
+                    gameWinnerMessage="Computer wins!"
+                }
+                else{
+                    gameWinnerMessage="Player wins!"
+                }
+            gameResult.innerText=gameWinnerMessage
+            }
+        } )
     }
-    console.log("GAME OVER")
-    let gameWinnerMessage
-    if (computerPoints==playerPoints){
-        gameWinnerMessage="Tie! No winner."
-    }
-    else if (computerPoints>playerPoints){
-        gameWinnerMessage="Computer wins!"
-    }
-    else{
-        gameWinnerMessage="Player wins!"
-    }
-    return gameWinnerMessage
 }
-console.log(game())
+game()
+
